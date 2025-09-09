e04e2fcc733eb9d83870406676959a58395ef505
ll-nick <github@le-large.net>
2025-09-09 21:25:35 +0200

    Format keymap via qmk.nvim and add lazy config

---
+++ .lazy.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "codethread/qmk.nvim",
+    ft = { "c", "dts" }, -- QMK uses C, ZMK uses devicetree (ft=dts)
+    opts = {
+      name = "lily58_layout", -- arbitrary name for zmk
+      variant = "zmk",
+      layout = {
+        -- left half
+        "x x x x x x _ _ x x x x x x",
+        "x x x x x x _ _ x x x x x x",
+        "x x x x x x _ _ x x x x x x",
+        "x x x x x x x x x x x x x x",
+        "_ _ _ x x x x x x x x _ _ _",
+      },
+    },
+  },
+}
